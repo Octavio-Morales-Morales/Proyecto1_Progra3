9d8d94ce2c3f2e258bb0448ef07c90fb12a55f9e
Octavio-Morales-Morales <moralesoctavio639@gmail.com>
2026-09-18 12:25:12 -0600

    commit2

--- src/main/java/DataAccesObject/ConexionBD.java
+++ src/main/java/DataAccesObject/ConexionBD.java
@@ -3,11 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DataAccesObject;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author COTO
  */
 public class ConexionBD {
+    private static final String URL = "jdbc:mysql://pma.us.cloudlogin.co:3306/gamabasis_p3g1?useSSL=false&serverTimezone=UTC";
+    private static final String USUARIO = "TU_USUARIO_DE_BASE_DE_DATOS"; 
+    private static final String CONTRASENIA = "TU_CONTRASENIA_DE_BASE_DE_DATOS";
     
+    public static Connection getConexion() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Driver no encontrado: " + e.getMessage());
+        }
+        return DriverManager.getConnection(URL, USUARIO, CONTRASENIA);
+    }
 }
